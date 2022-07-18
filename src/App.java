0ef3f41282d3b94e9d1f46abed013d158265db53
Felicio Gabriel <feliciogs@hotmail.com>
2022-07-18 15:58:13 -0300

    Primeiro commit

---
+++ src/App.java
@@ -0,0 +1,32 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Iniciar conexão HTTP
+        String apiKey = "k_nnxgj0mm";
+        String url = "https://imdb-api.com/en/API/Top250Movies/"+apiKey;
+        URI endereco = URI.create(url);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
+
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+
+        String body = response.body();
+        
+        JsonParser parser = new JsonParser();
+        List<Map<String,String>> listaDeFilmes = parser.parse(body);
+
+        for (Map<String,String> filme : listaDeFilmes) {
+            System.out.println(filme.get("title")); 
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+        }
+    }
+}
